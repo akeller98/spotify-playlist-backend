d177e6a684445b2c71a22795dca7000563047c9c
akeller98 <aaronkeller2021@u.northwestern.edu>
2019-03-31 00:40:18 -0500

    edited scopes 3/31/19 @ 12:40AM

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let querystring = require('querystring')
 
 let app = express()
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
+let redirect_uri =
+  process.env.REDIRECT_URI ||
   'http://localhost:8888/callback'
 
 app.get('/login', function(req, res) {
@@ -13,7 +13,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email playlist-modify-public app-remote-control playlist-read-private',
       redirect_uri
     }))
 })
